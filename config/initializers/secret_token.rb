6838c62876ab65c535260ed5ff550372a84767e3
Christopher Wong <christopher.cl.wong@gmail.com>
2012-09-25 23:39:37 -0400

    first commit of musteatpie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musteatpie::Application.config.secret_token = '1b04c98e07f354e3bb1e130c2e5831f2f79a0b112da10a33e2ad9f86f62da9efa970358cb92bd0349eff618974fee06d0b4257b8ba9a45877de68542a843d4b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Musteatpie::Application.config.session_store :cookie_store, key: '_musteatpie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Musteatpie::Application.config.session_store :active_record_store
