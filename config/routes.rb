2a86812ddb5a7fe1599f3d492430b9306d49525b
Christopher Wong <christopher.cl.wong@gmail.com>
2012-09-30 13:11:08 -0400

    putting in basics of first pages (home, about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Musteatpie::Application.routes.draw do
+  root to: 'static_pages#home'
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+
+  match 'about', to: 'static_pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
